554caa6ce0719128696151cc493d74cee006aa72
DESKTOP-9B4BOP3\STL <inimranali48@gmail.com>
2020-10-06 10:21:28 +0600

    update

--- SolutionBD.Domain/ResponseModel.cs
+++ SolutionBD.Domain/ResponseModel.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace SolutionBD.Domain
 {
-    public class ResponseModel
+    public class ListResponseModel<TModel> : IListResponseModel<TModel>
     {
+
+        public int Index { set; get; }
+        public int Count { set; get; }
+        public int TotalCount { set; get; }
+        public IEnumerable<TModel> Model { get; set; }
+    }
+    public class SingleResponseModel<TModel> : ISingleResponseModel<TModel>
+    {
+        public TModel Model { get; set; }
+    }
+    public interface ISingleResponseModel<TModel>
+    {
+        TModel Model { get; set; }
+    }
+
+    public interface IListResponseModel<TModel>
+    {
+        IEnumerable<TModel> Model { get; set; }
     }
 }
